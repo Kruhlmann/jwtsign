c3069afa152320e97043ec879f20677682063da8
Kristian Jakobsen <kristian@nymann.dev>
2023-08-15 20:46:11 +0200

    :poop: Add temporary build-wheels.sh script
    docker run -v `pwd`:/io quay.io/pypa/manylinux_2_28_x86_64 /io/build-wheels.sh

---
+++ build-wheels.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env sh
+
+cd /io || exist 1
+
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+export PATH="/root/.cargo/bin:$PATH"
+
+for PYBIN in /opt/python/cp3*/bin; do
+    "${PYBIN}/pip" install wheel setuptools-rust
+    "${PYBIN}/python3" setup.py sdist bdist_wheel
+done
+
+mkdir -p wheelhouse
+mv dist/*.whl wheelhouse/
+
+for whl in wheelhouse/*.whl; do
+    auditwheel repair "$whl" --plat manylinux_2_28_x86_64 -w wheelhouse/
+done
