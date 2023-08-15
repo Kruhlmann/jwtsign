69579258cbf2b39c8823d14c2ed831cf89123265
Kristian Jakobsen <kristian@nymann.dev>
2023-08-15 22:45:03 +0200

    Add git tag versioning

--- make/build.mk
+++ make/build.mk
@@ -1,9 +1,12 @@
 .PHONY: build
 build: $(OUTDIR)/release/libjwtsign.so
 
+${PY_VERSION_FILE}:
+	@echo "__version__ = \"$(shell git describe --tag --always | grep -oE '[0-9]+\.[0-9]+\.[0-9]+' || echo "0.0.0")\"" > ${PY_VERSION_FILE}
+
 $(OUTDIR)/release/libjwtsign.so: $(RUST_SRC) Cargo.toml
 	$(CARGO) build --release
 
-dist:
+dist: ${PY_VERSION_FILE}
 	@pip install wheel setuptools-rust
 	$(PYTHON) setup.py sdist bdist_wheel
--- setup.py
+++ setup.py
@@ -1,15 +1,14 @@
+from distutils import util
+
 from setuptools import setup
 from setuptools_rust import Binding, RustExtension
 
+version: dict[str, str] = {}
+version_file_path = util.convert_path("src/version.py")
+with open(version_file_path) as version_file:
+    exec(version_file.read(), version)  # noqa: S102 DUO105, WPS421
+
 setup(
-    name="jwtsign",
-    version="1.0.1",
-    classifiers=[
-        "Programming Language :: Python",
-        "Programming Language :: Rust",
-    ],
-    packages=[],
-    rust_extensions=[RustExtension("jwtsign", binding=Binding.PyO3)],
-    include_package_data=True,
-    zip_safe=False,
+    rust_extensions=[RustExtension(target="jwtsign", binding=Binding.PyO3)],
+    version=version["__version__"],
 )
